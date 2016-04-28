d4ef5b7c19f28540f516e695b3f9885e6ce9aaae
NgesBrian <nges.brian@gmail.com>
2016-04-28 23:55:24 +0100

    testing new branch

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int 
+    main(){
+        int a, b;
+        printf("input the first number to be summed\n");
+        scanf("%d",&a);
+        print("input the last number to be summed\n");
+        scanf("%d",&b);
+        a=a+b;
+        printf("the sum of the two numbers is %d\n",a);
+        return 0;
+    }
